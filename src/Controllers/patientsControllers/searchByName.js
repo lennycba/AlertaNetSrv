ec816c054346c1eb3bb447d1675c4a233937e3e1
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-12-14 19:22:48 -0300

    test de name

--- src/Controllers/patientsControllers/searchByName.js
+++ src/Controllers/patientsControllers/searchByName.js
@@ -1,21 +1,46 @@
-const {Patient} = require('../../db');
-const {Op} = require('sequelize');
+const { Patient } = require("../../db");
+const { Op } = require("sequelize");
 
+const searchByName = async (name, lastName) => {
+  let whereCondition;
 
-const searchByName = async(name,lastName) =>{
-    const patients = await Patient.findAll({
-        where:{
-            [Op.and]:[
-                {name:{[Op.iLike]:`%${name}%`}},
-                {lastName:{[Op.iLike]:`%${lastName}%`}},
-            ]
-        }
-    })
-console.log('patientssssssssssssssssssssssssssss',patients)
-    //if(patients > 0) 
-    return patients;
-    //else throw Error('los datos no coinciden con ningun afiliado');
+  if (name && lastName) {
+    whereCondition = {
+      [Op.and]: [{ name: { [Op.iLike]: `%${name}%` } }, { lastName: { [Op.iLike]: `%${lastName}%` } }],
+    };
+  } else if (name || lastName) {
+    whereCondition = {
+      [Op.or]: [
+        { name: { [Op.iLike]: `%${name || lastName}%` } },
+        { lastName: { [Op.iLike]: `%${name || lastName}%` } },
+      ],
+    };
+  } else {
+    throw new Error("Se debe proporcionar al menos un nombre o un apellido.");
+  }
+
+  const patients = await Patient.findAll({
+    where: whereCondition,
+  });
+
+  return patients;
 };
 
+module.exports = searchByName;
 
-module.exports =searchByName;
+// const searchByName = async(name,lastName) =>{
+//     const patients = await Patient.findAll({
+//         where:{
+//             [Op.and]:[
+//                 {name:{[Op.iLike]:`%${name}%`}},
+//                 {lastName:{[Op.iLike]:`%${lastName}%`}},
+//             ]
+//         }
+//     })
+// console.log('patientssssssssssssssssssssssssssss',patients)
+//     //if(patients > 0)
+//     return patients;
+//     //else throw Error('los datos no coinciden con ningun afiliado');
+// };
+
+module.exports = searchByName;
--- src/db.js
+++ src/db.js
@@ -35,12 +35,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const {
-Alert,
-Personal,
-Mobile,
-Patient,
-} = sequelize.models;
+const { Alert, Personal, Mobile, Patient } = sequelize.models;
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
