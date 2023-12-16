2ee70dc72272a9902aa67ba8cd8b97b2f2042cfa
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-12-15 21:28:50 -0300

    postAlert, no se tienen en cuenta las propiedades que contienen id, se modifico el modelo alert para que los id no sean obligatorios asi deja crear

--- src/Routes/alertRouter.js
+++ src/Routes/alertRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const alertRouter = Router();
-const getAlerts = require('../Handlers/alertsHandlers/getAlerts');
-const getById = require('../Handlers/gralHandlers/getById');
+const getAlerts = require("../Handlers/alertsHandlers/getAlerts");
+const getById = require("../Handlers/gralHandlers/getById");
+const createTicket = require("../Handlers/alertsHandlers/createTicket");
 
 //alertRouter.post('/', createTicket);
-alertRouter.get('/', getAlerts);
-alertRouter.get('/:id', getById);
+alertRouter.get("/", getAlerts);
+alertRouter.get("/:id", getById);
+alertRouter.post("/", createTicket);
 
 module.exports = alertRouter;
